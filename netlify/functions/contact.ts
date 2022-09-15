a59357fc74daa7c5bdddac81e431653ec2824f11
Laurent <9209206+kodelio@users.noreply.github.com>
2022-09-15 13:19:16 +0200

    fix netlify function

--- netlify/functions/contact.ts
+++ netlify/functions/contact.ts
@@ -7,65 +7,58 @@ const mailjet = new Mailjet({
   apiSecret: process.env.MAILJET_API_SECRET,
 })
 
-const handler: Handler = (event) => {
+const handler: Handler = async (event) => {
   if (event.httpMethod === 'POST' && event.body) {
     const { firstname, lastname, email, message, token } = JSON.parse(
       event.body
     )
     if (token) {
-      axios
-        .post(
-          `https://recaptchaenterprise.googleapis.com/v1/projects/${process.env.RECAPTCHA_PROJECT_ID}/assessments?key=${process.env.RECAPTCHA_API_KEY}`,
-          {
-            event: {
-              token,
-              siteKey: process.env.RECAPTCHA_KEY,
-              expectedAction: 'FORM_EMAIL_SUBMIT',
-            },
-          }
-        )
-        .then((response) => {
-          if (response.data.tokenProperties.valid) {
-            const data: sendMailjet.IBody = {
-              Messages: [
+      const { data } = await axios.post(
+        `https://recaptchaenterprise.googleapis.com/v1/projects/${process.env.RECAPTCHA_PROJECT_ID}/assessments?key=${process.env.RECAPTCHA_API_KEY}`,
+        {
+          event: {
+            token,
+            siteKey: process.env.RECAPTCHA_KEY,
+            expectedAction: 'FORM_EMAIL_SUBMIT',
+          },
+        }
+      )
+      if (data.tokenProperties.valid) {
+        const data: sendMailjet.IBody = {
+          Messages: [
+            {
+              From: {
+                Email: 'laurent@kodelio.fr',
+              },
+              To: [
                 {
-                  From: {
-                    Email: 'laurent@kodelio.fr',
-                  },
-                  To: [
-                    {
-                      Email: 'laurent@kodelio.fr',
-                    },
-                  ],
-                  Headers: { 'Reply-To': email },
-                  Subject: 'Nouveau message sur kodelio.com',
-                  HTMLPart: `Nom : <b>${firstname} ${lastname}</b><br />Adresse e-mail : ${email}<br />Message :<br /> ${message}`,
-                  TextPart: `Nom : ${firstname} ${lastname}\nAdresse e-mail : ${email}\nMessage :\n ${message}`,
+                  Email: 'laurent@kodelio.fr',
                 },
               ],
-            }
+              Headers: { 'Reply-To': email },
+              Subject: 'Nouveau message sur kodelio.com',
+              HTMLPart: `Nom : <b>${firstname} ${lastname}</b><br />Adresse e-mail : ${email}<br />Message :<br /> ${message}`,
+              TextPart: `Nom : ${firstname} ${lastname}\nAdresse e-mail : ${email}\nMessage :\n ${message}`,
+            },
+          ],
+        }
 
-            mailjet
-              .post('send', { version: 'v3.1' })
-              .request({ ...data })
-              .then((result) => {
-                const { Status } = (
-                  result.body as unknown as sendMailjet.IResponse
-                ).Messages[0]
-                return {
-                  statusCode: 200,
-                  body: JSON.stringify(Status),
-                }
-              })
-              .catch((err) => {
-                return {
-                  statusCode: 500,
-                  body: JSON.stringify(err),
-                }
-              })
-          }
-        })
+        const result = await mailjet
+          .post('send', { version: 'v3.1' })
+          .request({ ...data })
+
+        const { Status } = (result.body as unknown as sendMailjet.IResponse)
+          .Messages[0]
+        return {
+          statusCode: 200,
+          body: JSON.stringify(Status),
+        }
+      }
     }
+  }
+  return {
+    statusCode: 500,
+    body: JSON.stringify('error'),
   }
 }
 
